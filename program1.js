eecbdfbd2582c25743e8096be455c25ecaa5715c
kashi1230 <152697246+kashi1230@users.noreply.github.com>
2024-02-23 13:40:05 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,6 +3,21 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+     const romanValues = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
+    let total = 0;
+    let prevValue = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        const value = romanValues[s[i]];
+        if (value > prevValue) {
+            total += value - 2 * prevValue; 
+        } else {
+            total += value;
+        }
+        prevValue = value;
+    }
+    
+    return total;
     
 };
 
