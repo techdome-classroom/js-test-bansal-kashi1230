3fdd3d66d52d6342408df93adb15d19b3285c392
kashi1230 <152697246+kashi1230@users.noreply.github.com>
2024-02-23 13:40:26 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,6 +3,26 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const stack = [];
+    const pairs = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
+    
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (pairs[char]) { 
+            stack.push(char);
+        } else { 
+            const lastBracket = stack.pop();
+            if (pairs[lastBracket] !== char) {
+                return false;
+            }
+        }
+    }
+    
+    return stack.length === 0;
     
 };
 
